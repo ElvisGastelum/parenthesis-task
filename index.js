647a1b0e10fcdc4ea9b4f824654b32c10fb7ca5c
Elvis Gastelum <elvisgastelum@outlook.com>
2020-09-29 10:37:47 -0700

    implement funtionality to pass the test

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 function isBalanced(str) {
-  // @todo implement
+  const stringSplitted = str.split('');
+  var temp = [];
+
+  stringSplitted.forEach((currentCharacter) => {
+    if (isOpenCharacter(currentCharacter)) {
+      temp = [...temp, currentCharacter];
+    }
+
+    if (
+      isCloseCharacter(currentCharacter) &&
+      isInOrder(temp.last(), currentCharacter)
+    ) {
+      temp = deleteLastItem(temp);
+    }
+  });
+  return temp.isEmpty();
+}
+
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
+
+Array.prototype.isEmpty = function () {
+  return this.length === 0;
+};
+
+function isOpenCharacter(character) {
+  const validCharacters = ['(', '{', '['];
+  return isValidCharacter(character, validCharacters);
+}
+
+function isCloseCharacter(character) {
+  const validCharacters = [')', '}', ']'];
+  return isValidCharacter(character, validCharacters);
+}
+
+function isValidCharacter(character, validCharacters) {
+  const charactersFiltered = validCharacters.filter(
+    (value) => character === value
+  );
+  return !charactersFiltered.isEmpty();
+}
+
+function isInOrder(firstCharacter, secondCharacter) {
+  return (
+    (firstCharacter === '(' && secondCharacter === ')') ||
+    (firstCharacter === '{' && secondCharacter === '}') ||
+    (firstCharacter === '[' && secondCharacter === ']')
+  );
+}
+
+function deleteLastItem(array) {
+  return array.slice(0, -1);
 }
 
 module.exports = {
-  isBalanced
-}
+  isBalanced,
+};
