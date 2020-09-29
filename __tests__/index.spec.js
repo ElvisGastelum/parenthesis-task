3acfb8bb85c23b89e80c533f0706e34c4a004236
Elvis Gastelum <elvisgastelum@outlook.com>
2020-09-29 10:25:06 -0700

    delete skip and correction to name in test

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -23,13 +23,13 @@ describe('isBalanced with only parenthesis', () => {
     })
   })
 
-  describe.skip('with other characters', () => {
+  describe('with other characters', () => {
     it('should return true if the characters are even and nested', () => {
       const result = isBalanced('([{}])')
       expect(result).toBe(true)
     })
 
-    it('should return true if the characters are not even', () => {
+    it('should return false if the characters are not even', () => {
       const result = isBalanced('()[]{')
       expect(result).toBe(false)
     })
